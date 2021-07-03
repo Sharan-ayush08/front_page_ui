f9ea76a45e563a873e0551317dfabffc9d24e8b7
Sharan-ayush08 <ayushsharan22@gmail.com>
2021-07-03 11:43:21 -0700

    Some Changes Done

--- lib/screens/front_page.dart
+++ lib/screens/front_page.dart
@@ -17,97 +17,98 @@ class _FrontpageState extends State<Frontpage> {
         appBar: AppBar(
           title: Text("MEDICAL SERVICES"),
         ),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [Colors.blue, Colors.red, Colors.blue, Colors.white],
-            ),
-          ),
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 25, left: 20, right: 20),
-                height: 60.0,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                        color: Color(0x14000000),
-                        offset: Offset(0, 10),
-                        blurRadius: 15,
-                        spreadRadius: 0),
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 5,
-                      child: Container(
-                        margin: EdgeInsets.only(left: 10, right: 10),
-                        child: TextField(
-                          maxLines: 1,
-                          autofocus: false,
-                          style: TextStyle(
-                              color: Color(0xff1071613), fontSize: 20),
-                          decoration: InputDecoration(
-                              border: InputBorder.none, hintText: "Search"),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Color(0xff1071613),
-                            borderRadius: BorderRadius.circular(5)),
-                        child: Center(
-                          child:
-                              Icon(Icons.search, color: Colors.white, size: 25),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+        body: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Colors.blue, Colors.red, Colors.blue, Colors.white],
               ),
-              Container(
-                width: double.infinity,
-                margin: EdgeInsets.only(top: 20, left: 20),
-                child: Stack(
-                  children: [
-                    Container(
-                      child: Text(
-                        "Category",
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.w700),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 20),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "Show all",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontFamily: 'Roboto',
+            ),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 25, left: 20, right: 20),
+                  height: 60.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color(0x14000000),
+                          offset: Offset(0, 10),
+                          blurRadius: 15,
+                          spreadRadius: 0),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: Container(
+                          margin: EdgeInsets.only(left: 10, right: 10),
+                          child: TextField(
+                            maxLines: 1,
+                            autofocus: false,
+                            style: TextStyle(
+                                color: Color(0xff1071613), fontSize: 20),
+                            decoration: InputDecoration(
+                                border: InputBorder.none, hintText: "Search"),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Color(0xff1071613),
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Center(
+                            child: Icon(Icons.search,
+                                color: Colors.white, size: 25),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              PageElements(),
-            ],
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 20, left: 20),
+                  child: Stack(
+                    children: [
+                      Container(
+                        child: Text(
+                          "Category",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 20),
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            "Show all",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                PageElements(),
+              ],
+            ),
           ),
         ),
         bottomNavigationBar: BottomNavyBar(
--- lib/screens/page_elements.dart
+++ lib/screens/page_elements.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:front_page_ui/screens/drugs.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PageElements extends StatefulWidget {
   const PageElements({Key? key}) : super(key: key);
@@ -9,79 +10,145 @@ class PageElements extends StatefulWidget {
 }
 
 class _PageElemntsState extends State<PageElements> {
-  final List<String> _services = [
-    "Medicines",
-    "Shots",
-    "Consulting",
-    "Ambulance",
-    "Nurse",
-    "LabWork",
-    "More"
-  ];
-  final _iconTypes = <IconData>[
-    Icons.medication_rounded,
-    Icons.medical_services,
-    Icons.person,
-    Icons.car_rental_outlined,
-    Icons.local_hospital,
-    Icons.house,
-    Icons.more
-  ];
+  // final List<String> _services = [
+  //   "Medicines",
+  //   "Shots",
+  //   "Consulting",
+  //   "Ambulance",
+  //   "Nurse",
+  //   "LabWork",
+  //   "More"
+  // ];
+  // final _iconTypes = <IconData>[
+  //   Icons.medication_rounded,
+  //   Icons.medical_services,
+  //   Icons.person,
+  //   Icons.car_rental_outlined,
+  //   Icons.local_hospital,
+  //   Icons.house,
+  //   Icons.more
+  // ];
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: EdgeInsets.all(9.0),
-        child: GridView.builder(
-          itemCount: _services.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 20.0,
-              mainAxisSpacing: 15.0,
-              childAspectRatio: 1.2,
-              mainAxisExtent: 80.0),
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                setState(() {
-                  if (_services[index] == 'Medicines') {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ViewMedicines()),
-                    );
-                  }
-                });
-              },
-              child: Container(
-                // width: 100.0,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.green),
-                child: Card(
-                  color: Colors.black,
-                  child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          _iconTypes[index],
-                          color: Colors.white,
-                          size: 30.0,
-                        ),
-                        Text(
-                          _services[index],
-                          style: TextStyle(color: Colors.white, fontSize: 15.0),
-                        ),
-                      ],
-                    ),
-                  ),
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Card(
+              color: Colors.transparent,
+              child: ListTile(
+                leading: Icon(
+                  Icons.medical_services,
+                  color: Colors.white,
+                ),
+                title: Text('Medicines',
+                    style: GoogleFonts.acme(fontSize: 20, color: Colors.white)),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.white,
                 ),
               ),
-            );
-          },
-        ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Card(
+              color: Colors.transparent,
+              child: ListTile(
+                leading: Icon(
+                  Icons.medication_outlined,
+                  color: Colors.white,
+                ),
+                title: Text('Shots',
+                    style: GoogleFonts.acme(fontSize: 20, color: Colors.white)),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Card(
+              color: Colors.transparent,
+              child: ListTile(
+                leading: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+                title: Text('Consulting',
+                    style: GoogleFonts.acme(fontSize: 20, color: Colors.white)),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Card(
+              color: Colors.transparent,
+              child: ListTile(
+                leading: Icon(
+                  Icons.horizontal_split_sharp,
+                  color: Colors.white,
+                ),
+                title: Text('Ambulance',
+                    style: GoogleFonts.acme(fontSize: 20, color: Colors.white)),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Card(
+              color: Colors.transparent,
+              child: ListTile(
+                leading: Icon(
+                  Icons.local_hospital,
+                  color: Colors.white,
+                ),
+                title: Text('Nurse',
+                    style: GoogleFonts.acme(fontSize: 20, color: Colors.white)),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Card(
+              color: Colors.transparent,
+              child: ListTile(
+                leading: Icon(
+                  Icons.local_hospital,
+                  color: Colors.white,
+                ),
+                title: Text('LabWork',
+                    style: GoogleFonts.acme(fontSize: 20, color: Colors.white)),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          // ListTile(
+          //   trailing: Icon(
+          //     Icons.more_horiz,
+          //     size: 50.0,
+          //   ),
+          // )
+        ],
       ),
     );
   }
