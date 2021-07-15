8be373253b15ace453d5e3c50512c1a3cb33ce58
Sharan-ayush08 <ayushsharan22@gmail.com>
2021-07-15 12:28:11 -0700

    changes applied

--- lib/screens/front_page.dart
+++ lib/screens/front_page.dart
@@ -10,136 +10,83 @@ class Frontpage extends StatefulWidget {
 }
 
 class _FrontpageState extends State<Frontpage> {
+  final List<String> _listItem = [
+    "assets/icons/general_practice.png",
+    "assets/icons/immunisation.png",
+    "assets/icons/specialist.png",
+    "assets/icons/logo.png",
+    "assets/icons/sexual_health.png",
+  ];
+  final List<String> _names = [
+    "Genral Practice",
+    "Immunisation",
+    "Specialist",
+    "Meds",
+    "Health",
+  ];
   int currentBarItem = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("MEDICAL SERVICES"),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
+      // backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[400],
+        elevation: 0,
+        title: Text("MEDICAL SERVICES"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
             decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [Colors.blue, Colors.red, Colors.blue, Colors.white],
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(70.0),
+                bottomRight: Radius.circular(70.0),
+              ),
+              image: DecorationImage(
+                image: AssetImage("assets/icons/medical.jpg"),
               ),
             ),
-            child: Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 25, left: 20, right: 20),
-                  height: 60.0,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                          color: Color(0x14000000),
-                          offset: Offset(0, 10),
-                          blurRadius: 15,
-                          spreadRadius: 0),
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 5,
-                        child: Container(
-                          margin: EdgeInsets.only(left: 10, right: 10),
-                          child: TextField(
-                            maxLines: 1,
-                            autofocus: false,
-                            style: TextStyle(
-                                color: Color(0xff1071613), fontSize: 20),
-                            decoration: InputDecoration(
-                                border: InputBorder.none, hintText: "Search"),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Color(0xff1071613),
-                              borderRadius: BorderRadius.circular(5)),
-                          child: Center(
-                            child: Icon(Icons.search,
-                                color: Colors.white, size: 25),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.only(top: 20, left: 20),
-                  child: Stack(
-                    children: [
-                      Container(
-                        child: Text(
-                          "Category",
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: 'Roboto',
-                              fontWeight: FontWeight.w700),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(right: 20),
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: Text(
-                            "Show all",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: 'Roboto',
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                PageElements(),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.only(top: 25.0, left: 35.0),
+              child: Text(
+                "Welcome...",
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
             ),
           ),
-        ),
-        bottomNavigationBar: BottomNavyBar(
-          selectedIndex: currentBarItem,
-          onItemSelected: (index) {
-            setState(() {
-              currentBarItem = index;
-            });
-          },
-          items: <BottomNavyBarItem>[
-            BottomNavyBarItem(
-                icon: Icon(Icons.home),
-                title: Text("Home"),
-                activeColor: Colors.blue,
-                inactiveColor: Colors.black),
-            BottomNavyBarItem(
-                icon: Icon(Icons.health_and_safety),
-                title: Text("HealthCare"),
-                activeColor: Colors.blue,
-                inactiveColor: Colors.black),
-            BottomNavyBarItem(
-                icon: Icon(Icons.search),
-                title: Text("Diagnostics"),
-                activeColor: Colors.blue,
-                inactiveColor: Colors.black),
-            BottomNavyBarItem(
-                icon: Icon(Icons.person),
-                title: Text("Account"),
-                activeColor: Colors.blue,
-                inactiveColor: Colors.black),
-          ],
-        ));
+          ListTile(
+            leading: Text(
+              "Our Health Services",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.blue),
+            ),
+            trailing: Icon(Icons.menu),
+          ),
+          Expanded(
+            child: Container(
+              child: GridView.count(
+                crossAxisCount: 3,
+                padding: EdgeInsets.all(18.0),
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                children: _listItem
+                    .map(
+                      (item) => Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(item), fit: BoxFit.cover)),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
