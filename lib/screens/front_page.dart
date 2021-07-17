9f0df86c258c63c87626c79fbccf55e2b998a1e7
Sharan-ayush08 <ayushsharan22@gmail.com>
2021-07-16 21:04:21 -0700

    Some changes made

--- lib/screens/front_page.dart
+++ lib/screens/front_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:front_page_ui/screens/drugs.dart';
 import 'package:front_page_ui/screens/page_elements.dart';
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 
@@ -10,80 +11,117 @@ class Frontpage extends StatefulWidget {
 }
 
 class _FrontpageState extends State<Frontpage> {
-  final List<String> _listItem = [
-    "assets/icons/general_practice.png",
-    "assets/icons/immunisation.png",
-    "assets/icons/specialist.png",
-    "assets/icons/logo.png",
-    "assets/icons/sexual_health.png",
+  final List _listItem = [
+    // Icon(Icons.local_hospital),
+    // Icon(Icons.medical_services),
+    // Icon(Icons.mediation),
+    // Icon(Icons.local_hospital),
+    // Icon(Icons.local_hospital),
+    // Icon(Icons.local_hospital),
+    // Icon(Icons.local_hospital),
+    // Icon(Icons.local_hospital),
+    Icons.local_hospital,
+    Icons.local_hospital,
+    Icons.local_hospital,
+    Icons.local_hospital,
+    Icons.local_hospital,
+    Icons.local_hospital,
+    Icons.local_hospital,
+    Icons.more_horiz,
   ];
-  final List<String> _names = [
-    "Genral Practice",
-    "Immunisation",
-    "Specialist",
-    "Meds",
-    "Health",
+  final List<String> _text = [
+    "health",
+    "hospital",
+    "medicine",
+    "health",
+    "hospital",
+    "medicine",
+    "health",
+    "More",
   ];
+
   int currentBarItem = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.blue,
       appBar: AppBar(
-        backgroundColor: Colors.blue[400],
         elevation: 0,
         title: Text("MEDICAL SERVICES"),
       ),
       body: Column(
         children: [
           Container(
-            height: 300,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(70.0),
-                bottomRight: Radius.circular(70.0),
-              ),
-              image: DecorationImage(
-                image: AssetImage("assets/icons/medical.jpg"),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 25.0, left: 35.0),
-              child: Text(
-                "Welcome...",
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          ListTile(
-            leading: Text(
-              "Our Health Services",
-              style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.blue),
-            ),
-            trailing: Icon(Icons.menu),
-          ),
-          Expanded(
             child: Container(
-              child: GridView.count(
-                crossAxisCount: 3,
-                padding: EdgeInsets.all(18.0),
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20,
-                children: _listItem
-                    .map(
-                      (item) => Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(item), fit: BoxFit.cover)),
-                      ),
-                    )
-                    .toList(),
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(offset: Offset(0, 1), blurRadius: 2),
+                ],
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
+                    hintText: "Search for medicines and Health care..."),
               ),
             ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            height: 180.0,
+            child: Expanded(
+              flex: 1,
+              child: GridView.builder(
+                itemCount: _listItem.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 15.0,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    child: Column(
+                      children: <Widget>[
+                        InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ViewMedicines()));
+                            },
+                            child: Icon(_listItem[index],
+                                size: 40.0, color: Colors.red)),
+                        Text(_text[index])
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.health_and_safety),
+            label: "Health Care",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search_rounded),
+            label: "Diagnostics",
           ),
         ],
       ),
